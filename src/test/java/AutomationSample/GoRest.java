8580ec36e9f022e3ba187bcd7c9e0d3df09a1d88
Tech Global <techglobal.school@gmail.com>
2022-02-23 18:38:57 -0600

    added Put and Delete requests

--- src/test/java/AutomationSample/GoRest.java
+++ src/test/java/AutomationSample/GoRest.java
@@ -27,7 +27,7 @@ public class GoRest {
                         "    \"email\": \"" + faker.internet().emailAddress() + "\",\n" +
                         "    \"status\": \"active\"\n" +
                         "}")
-                // sending the url with the user id
+                // sending the post URL
                 .when().post("https://gorest.co.in/public/v2/users")
                 // extracting the response
                 .then().extract().response();
@@ -37,6 +37,7 @@ public class GoRest {
 
         actualUserId = response.jsonPath().getInt("id");
         System.out.println("Getting the actual User Id: " + actualUserId);
+        String actualEmail = response.jsonPath().getString("email");
 
         // Sending get request to get single user
         response = RestAssured
@@ -67,5 +68,34 @@ public class GoRest {
         System.out.println("User id from the GET call response: " + responseId);
         Assert.assertEquals(actualUserId, responseId, "UserId is");
 
+        // Updating the user with the PUT method
+        response = RestAssured.given()
+                // header is smiler where in the postman
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer bf5065bb8a90de145d38ecbc2aec4744a1ca2c57f83b7a5be93061df4376911c")
+                .body("{\n" +
+                        "    \"name\": \"" + faker.name().fullName() + "\",\n" +
+                        "    \"email\": \"" + actualEmail + "\",\n" +
+                        "    \"status\": \"active\"\n" +
+                        "}")
+                .when().put("https://gorest.co.in/public/v2/users/" + actualUserId)
+                .then().extract().response();
+
+        // Getting the whole response body
+        String updatedResponseBody = response.getBody().asString();
+        System.out.println("PUT Response body is: " + updatedResponseBody);
+
+        // Deleting user with DELETE method
+
+        response = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer bf5065bb8a90de145d38ecbc2aec4744a1ca2c57f83b7a5be93061df4376911c")
+                .when().delete("https://gorest.co.in/public/v2/users/" + actualUserId)
+                .then().extract().response();
+
+        int deleteResponseStatusCode = response.statusCode();
+        System.out.println("Status code after deleting should be: " + deleteResponseStatusCode);
+
+        Assert.assertEquals(deleteResponseStatusCode, 204, "Status code after the delete is: ");
     }
 }
