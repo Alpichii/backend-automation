e8fa21ec0232e6fcc51162367c310695fdd71b23
Tech Global <techglobal.school@gmail.com>
2022-02-27 15:01:58 -0600

    continued ExcelUtils

--- src/test/java/utils/ExcelUtils.java
+++ src/test/java/utils/ExcelUtils.java
@@ -1,11 +1,15 @@
 package utils;
 
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExcelUtils {
 
@@ -16,10 +20,10 @@ public class ExcelUtils {
     private static String excelFilePath;
 
     public static void openExcelFile(String excelFileName, String sheetName) {
-        excelFileName = "testData/" + excelFileName + ".xlsx";
+        excelFilePath = "testData/" + excelFileName + ".xlsx";
 
         try {
-            FileInputStream fileInputStream = new FileInputStream(excelFileName);
+            FileInputStream fileInputStream = new FileInputStream(excelFilePath);
             workbook = new XSSFWorkbook(fileInputStream);
             sheet = workbook.getSheet(sheetName);
             System.out.println("The file and sheet exist!");
@@ -28,8 +32,78 @@ public class ExcelUtils {
         }
     }
 
+    public static String getValue(int rowNum, int cellNum) {
+        row = sheet.getRow(rowNum);
+        cell = row.getCell(cellNum);
+        cell.setCellType(CellType.STRING);
+        return cell.toString();
+    }
+
+    // This method will override the existing cell
+    // Because in the cell we are getting the existing cell
+    public static void setValue(String newValue, int rowNum, int cellNum) {
+        row = sheet.getRow(rowNum);
+        cell = row.getCell(cellNum);
+        cell.setCellValue(newValue);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(excelFilePath);
+            workbook.write(fileOutputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // This method is writing the value anywhere in the Excel sheet
+    // Because in the cell we are using create method for the row
+    public static void insertValue(String insertValue, int rowNum, int cellNum) {
+        row = sheet.createRow(rowNum);
+        cell = row.createCell(cellNum);
+        cell.setCellValue(insertValue);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(excelFilePath);
+            workbook.write(fileOutputStream);
+        } catch (Exception e) {
+            System.out.println("File is not exist to update!");
+        }
+    }
+
+    public static List<List<String>> getValues() {
+        List<List<String>> allValues = new ArrayList<>();
+        for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
+            row = sheet.getRow(i);
+            List<String> eachRow = new ArrayList<>();
+            for (int j = row.getFirstCellNum(); j < row.getLastCellNum(); j++) {
+                eachRow.add(getValue(i, j));
+            }
+            allValues.add(eachRow);
+        }
+        return allValues;
+    }
+
+    public static String[][] getExcelData(List<List<String>> mergedList) {
+        String[][] result = new String[mergedList.size()][];
+        for (int i = 0; i < mergedList.size(); i++) {
+            List<String> currentList = mergedList.get(i);
+            result[i] = currentList.toArray(new String[currentList.size()]);
+        }
+        return result;
+    }
+
+    public static void closeExcelFile() {
+        try {
+            workbook.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         openExcelFile("excelSample", "Sheet1");
+//        System.out.println(getValue(2, 3));
+//        setValue("Polo", 1, 0);
+//        insertValue("Polo", 7, 8);
+        System.out.println(getValues());
+
     }
 
 }
